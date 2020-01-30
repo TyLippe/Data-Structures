db6c00fb261376b4f3e92762f1fe284cfb8ed93a
Ty Lippe <tylippe94@gmail.com>
2020-01-30 16:14:53 -0500

    added day2 and stretch

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('../queue_and_stack')
-#from dll_stack import Stack
-
+from dll_stack import Stack
+from dll_queue import Queue
 
 class BinarySearchTree:
     def __init__(self, value):
@@ -12,7 +12,7 @@ class BinarySearchTree:
     # Insert the given value into the tree
     def insert(self, value):
         #if greater than or equal to we will move to the right
-        if value >= self.value: 
+        if value >= self.value:
             #set the current_node to the node to the right of current_node, recursive
             if self.right:
                 self.right.insert(value)
@@ -60,7 +60,6 @@ class BinarySearchTree:
         #recursive through tree until no more right
         if self.right:
             return self.right.get_max()
-        
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
@@ -73,53 +72,88 @@ class BinarySearchTree:
 
     # Messing with delete
     def delete(self, value):
-        if self.contains(value) is True:
-	        if value == self.value:
-	            if self.left:
-	                left_node = self.left                
-	                max_value = left_node.get_max()
-	                self.value = max_value
-	                left_node.delete(max_value)
-	            elif self.right:
-	                right_node = self.right
-	                self.value = right_node.value
-	                right_node.delete(right_node.value)
+            if self.contains(value) is True:
+	            if value == self.value:
+	                if self.left:
+	                    left_node = self.left
+	                    max_value = left_node.get_max()
+	                    self.value = max_value
+	                    left_node.delete(max_value)
+	                elif self.right:
+	                    right_node = self.right
+	                    self.value = right_node.value
+	                    right_node.delete(right_node.value)
+	                else:
+	                    return True
+	            elif value > self.value:
+	                delete = self.right.delete(value)
+	                if delete:
+	                    self.right = None
 	            else:
-	                return True
-	        elif value > self.value:
-	            delete = self.right.delete(value)
-	            if delete:
-	                self.right = None
-	        else:
-	            delete = self.left.delete(value)
-	            if delete:
-	                self.left = None
-
+	                delete = self.left.delete(value)
+	                if delete:
+	                    self.left = None
 
     # DAY 2 Project -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if self.left:
+            self.left.in_order_print(self)
+        print(self.value)
+        if self.right:
+            self.right.in_order_print(self)
+
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
-        pass
+        #create a queue
+        queue = Queue()
+        #add self to queue
+        queue.enqueue(self)
+        #while loop
+        while queue.size is not 0:
+            #dequeue head into temp var
+            temp = queue.dequeue()
+            print(temp.value)
+            #if temp.left add to queue
+            if temp.left:
+                queue.enqueue(temp.left)
+            #if temp.right add to queue
+            if temp.right:
+                queue.enqueue(temp.right)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
     def dft_print(self, node):
-        pass
+        #same as bft but stack
+        stack = Stack()
+        stack.push(self)
+        while stack.size is not 0:
+            temp = stack.pop()
+            print(temp.value)
+            if temp.left:
+                stack.push(temp.left)
+            if temp.right:
+                stack.push(temp.right)
 
     # STRETCH Goals -------------------------
     # Note: Research may be required
 
     # Print In-order recursive DFT
     def pre_order_dft(self, node):
-        pass
+        print(self.value)
+        if self.left:
+            self.left.pre_order_dft(self)
+        if self.right:
+            self.right.pre_order_dft(self)
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        if self.left:
+            self.left.post_order_dft(self)
+        if self.right:
+            self.right.post_order_dft(self)
+        print(self.value)
